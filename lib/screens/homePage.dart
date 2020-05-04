4dc1902f4a15f5615e2092b824b0bacdb8b56211
sayannath <sayannath235@gmail.com>
2020-05-05 03:09:58 +0530

    Home Page UI done

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -102,11 +102,58 @@ class _HomePageState extends State<HomePage> {
                 )
               ]),
       ),
-      body: Container(),
+      body: Container(
+        child: FirebaseAnimatedList(
+            query: _databaseReference,
+            itemBuilder: (BuildContext context, DataSnapshot snapshot,
+                Animation<double> animation, int index) {
+              return GestureDetector(
+                onTap: () {
+                  navigateToViewContacts(snapshot.key);
+                },
+                child: Card(
+                  color: Colors.white,
+                  elevation: 2.0,
+                  child: Container(
+                      margin: EdgeInsets.all(10.0),
+                      child: Row(
+                        children: <Widget>[
+                          Container(
+                            height: 50.0,
+                            width: 50.0,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                    image: snapshot.value['photoUrl'] == "empty"
+                                        ? AssetImage("assets/mascot.png")
+                                        : NetworkImage(
+                                            snapshot.value['photoUrl']))),
+                          ),
+                          Container(
+                            margin: EdgeInsets.all(10.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  "${snapshot.value['firstName']} ${snapshot.value['lastName']}",
+                                  style: TextStyle(
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: "Roboto"),
+                                ),
+                                SizedBox(height: 4),
+                                Text("${snapshot.value['phone']}")
+                              ],
+                            ),
+                          )
+                        ],
+                      )),
+                ),
+              );
+            }),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: navigateToAddContacts,
-        child: Icon(Icons.add)
-        ),
+          onPressed: navigateToAddContacts, child: Icon(Icons.add)),
     );
   }
 }
